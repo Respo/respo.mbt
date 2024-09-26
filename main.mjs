f145794780903c90cf14afbed29ce817a51efc09
tiye <jiyinyiyong@gmail.com>
2024-09-26 12:17:53 +0800

    get js code generated

--- main.mjs
+++ main.mjs
@@ -1,24 +1,24 @@
 // const wasmModuleUrl = "./target/wasm-gc/debug/build/lib/lib.wasm";
 // const wasmModuleUrl = "./target/wasm-gc/release/build/lib/lib.wasm";
 
-import wasmModuleUrl from "./target/wasm-gc/debug/build/lib/lib.wasm?url";
+// import wasmModuleUrl from "./target/wasm-gc/debug/build/lib/lib.wasm?url";
 
-async function loadWasmModule(url) {
-  try {
-    const response = await fetch(url);
-    const bytes = await response.arrayBuffer();
-    const results = await WebAssembly.instantiate(bytes, {
-      basic: {
-        jslog_1: (x) => console.log(x),
-      },
-    });
+// async function loadWasmModule(url) {
+//   try {
+//     const response = await fetch(url);
+//     const bytes = await response.arrayBuffer();
+//     const results = await WebAssembly.instantiate(bytes, {
+//       basic: {
+//         jslog_1: (x) => console.log(x),
+//       },
+//     });
 
-    const { onload } = results.instance.exports;
-    onload();
-  } catch (err) {
-    console.error("Error instantiating WebAssembly module:", err);
-  }
-}
+//     const { onload } = results.instance.exports;
+//     onload();
+//   } catch (err) {
+//     console.error("Error instantiating WebAssembly module:", err);
+//   }
+// }
 
-// Call the function with the URL of the Wasm module
-loadWasmModule(wasmModuleUrl);
+// // Call the function with the URL of the Wasm module
+// loadWasmModule(wasmModuleUrl);
